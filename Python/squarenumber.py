0979e99f6a4115351d1a479aa2bf9fb477e69b51
Pusetso Mguzulwa <92914430+pusetsom@users.noreply.github.com>
2022-11-23 23:44:06 +0200

    Update squarenumber.py

--- Python/squarenumber.py
+++ Python/squarenumber.py
@@ -2,7 +2,8 @@
 # .join - joins the list with no whitespaces
 string = input('Input a string name to check the length: ')
 if len(''.join(str(string))) == 0:
-    print("Empty string")
-else:
     space = string.split()
     print("{0} is {1}".format(string, str(len(''.join(space)))))
+else:
+    print("Empty string")
+    
